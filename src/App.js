96934855eb29034550fd65d20922739aa4913249
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-02-12 13:50:58 -0500

    initial commit
    A little late for initial commit but base of project implemented, plus tailwind and some components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react'
+import MainMenu from './Components/MainMenu'
+import Quiz from './Components/Quiz'
+import Results from './Components/Results'
+import { QuizContext } from './Helpers/Contexts'
+
 
 function App() {
+const [gameState, setGameState] = useState("menu")
+
+// setGameState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-white min-h-screen flex justify-center flex-col">
+      
+     <h1 className='flex justify-center text-3xl'>Quiz App</h1>
+
+     <QuizContext.Provider value={{gameState, setGameState}} >
+    {/* Example of short-circuiting to dynamically rendering display based on the state of the game. */}
+     {gameState === "menu" && <MainMenu />}
+     {gameState === "quiz" && <Quiz />}
+     {gameState === "results" && <Results />}
+     </QuizContext.Provider>
     </div>
   );
 }
---
+++ src/Helpers/JSQuestionsBank.js
@@ -0,0 +1,30 @@
+export const JSQuestions = [
+    // Question 1
+    {
+        prompt: "Which of the following statements about JS is true?",
+        optionA: "JS is a dynamically typed language",
+        optionB: "JS is a statically typed language",
+        optionC: "JS is a general programming language",
+        optionD: "Both A and C",
+        answer: "optionD"
+    },
+    // Question 2
+    {
+        prompt: "How many threads does JS support?",
+        optionA: "1",
+        optionB: "2",
+        optionC: "12",
+        optionD: "None of the above",
+        answer: "option1"
+    },
+    // Question 3
+    {
+        prompt: "Which operator returns true if the two compared values are not equal?",
+        optionA: "<>",
+        optionB: "!==",
+        optionC: "==!",
+        optionD: "~",
+        answer: "optionB"
+    },
+
+]
