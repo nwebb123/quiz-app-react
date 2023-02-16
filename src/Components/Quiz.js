2973523e01df1725a17dbcc7af928c4f00132c3e
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-02-15 23:13:59 -0500

    update
    Fixed edge cases/minor bugs for quiz. Next step is to polish UI, deploy to netlify and add readme, then add to portfolio

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -2,7 +2,6 @@ import { React, useState, useContext } from "react";
 import { QuizContext } from "../Helpers/Contexts";
 import { JSQuestions } from "../Helpers/JSQuestionsBank";
 
-
 function Quiz() {
   //Destructuring gamestate and set gamestate function from Quiz context.
   const { setGameState } = useContext(QuizContext);
@@ -14,22 +13,21 @@ function Quiz() {
   const [questionAnswered, setQuestionAnswered] = useState(false);
   const [isLastQuestion, setIsLastQuestion] = useState(false);
 
-
-  const selectOption = option => {
-  
+  const selectOption = (option) => {
     setOptionChosen(option);
     //console log below will not display the new value of optionChosen immediately after selection. That's why I had to pass in option (parameter) below instead of optionChosen (state)
     //console.log(optionChosen)
-    if (JSQuestions[currentQuestion].answer === option) {
-      //Cool feature would be to change background color of correct option to light-green and incorrect option to light-red.
-      console.log("Correct!");
-      setScore(prevScore => (
-        prevScore + 1
-        ))
-    } else {
-      //Change background color to red for all options that don't meet first condition.
-      console.log("Incorrect");
-    }
+
+    // if (JSQuestions[currentQuestion].answer === option) {
+    //   //Cool feature would be to change background color of correct option to light-green and incorrect option to light-red.
+    //   console.log("Correct!");
+    //   setScore(prevScore => (
+    //     prevScore + 1
+    //     ))
+    // } else {
+    //   //Change background color to red for all options that don't meet first condition.
+    //   console.log("Incorrect");
+    // }
     //console.log(score)
     setQuestionAnswered(true);
 
@@ -38,11 +36,22 @@ function Quiz() {
     }
   };
 
+  const submitOption = () => {
+    if (JSQuestions[currentQuestion].answer === optionChosen) {
+      //Cool feature would be to change background color of correct option to light-green and incorrect option to light-red.
+      console.log("Correct!");
+      setScore((prevScore) => prevScore + 1);
+    } else {
+      //Change background color to red for all options that don't meet first condition.
+      console.log("Incorrect");
+    }
+  };
+
   return (
     <div className="Quiz m-3 flex-col">
       <div className="py-10 mx-auto max-w-3xl justify-center bg-purple-500 rounded-xl">
         <h1 className="text-white text-xl text-center">
-          {JSQuestions[currentQuestion].prompt}
+          {JSQuestions[currentQuestion].question}
         </h1>
         <br />
         <div className="options p-3 flex flex-col text-white text-start">
@@ -58,24 +67,31 @@ function Quiz() {
           >
             B) {JSQuestions[currentQuestion].optionB}
           </button>
-          <button
-            className="bg-purple-400 m-2 p-2 rounded-md"
-            onClick={() => selectOption("optionC")}
-          >
-            C) {JSQuestions[currentQuestion].optionC}
-          </button>
-          <button
-            className="bg-purple-400 m-2 p-2 rounded-md"
-            onClick={() => selectOption("optionD")}
-          >
-            D) {JSQuestions[currentQuestion].optionD}
-          </button>
+
+          {/* If true/false, do not display */}
+          {JSQuestions[currentQuestion].isMultChoice && (
+            <>
+              <button
+                className="bg-purple-400 m-2 p-2 rounded-md"
+                onClick={() => selectOption("optionC")}
+              >
+                C) {JSQuestions[currentQuestion].optionC}
+              </button>
+              <button
+                className="bg-purple-400 m-2 p-2 rounded-md"
+                onClick={() => selectOption("optionD")}
+              >
+                D) {JSQuestions[currentQuestion].optionD}
+              </button>
+            </>
+          )}
 
           {/* See Reults button render */}
           {questionAnswered && isLastQuestion && (
             <button
               onClick={() => {
-               setGameState("results")
+                submitOption();
+                setGameState("results");
               }}
               className="p-3 mt-6 mx-auto  bg-purple-700 text-white rounded-sm"
             >
@@ -83,12 +99,13 @@ function Quiz() {
             </button>
           )}
 
-              {/* Next Question button render */}
+          {/* Next Question button render */}
           {questionAnswered && !isLastQuestion && (
             <button
               onClick={() => {
+                submitOption();
+                //Increment state of currentQuestion along with resetting whether or not the question is answered (from true to false)
                 setCurrentQuestion((prevCurrentQuestion) => {
-                  //Need to account for moving to result page after last question.
                   setQuestionAnswered(false);
                   return prevCurrentQuestion + 1;
                 });
@@ -98,7 +115,6 @@ function Quiz() {
               Next Question
             </button>
           )}
-
         </div>
       </div>
     </div>
--- src/Helpers/JSQuestionsBank.js
+++ src/Helpers/JSQuestionsBank.js
@@ -1,91 +1,101 @@
 export const JSQuestions = [
     // Question 1
     {
-        prompt: "Which of the following statements about Javascript is true?",
+        question: "Which of the following statements about Javascript is true?",
         optionA: "Javascript is a dynamically typed language",
         optionB: "Javascript is a statically typed language",
         optionC: "Javascript is a general-purpose programming language",
         optionD: "Both A and C",
-        answer: "optionD"
+        answer: "optionD",
+        isMultChoice: true,
     },
     // Question 2
     {
-        prompt: "True of false: Javascript is a multi-threaded language.",
+        question: "True of false: Javascript is a multi-threaded language.",
         optionA: "True",
         optionB: "False",       
-        answer: "optionB"
+        answer: "optionB",
+        isMultChoice: false,
     },
     // Question 3
     {
-        prompt: "Which operator is used to express whether two compared values are NOT equal?",
+        question: "Which operator is used to express whether two compared values are NOT equal?",
         optionA: "<>",
         optionB: "!==",
         optionC: "==!",
         optionD: "~",
-        answer: "optionB"
+        answer: "optionB",
+        isMultChoice: true
     },
     // Question 4
     {
-        prompt: "Which of the following keywords is used to define a variable that cannot be reassigned?",
+        question: "Which of the following keywords is used to define a variable that cannot be reassigned?",
         optionA: "var",
         optionB: "let",
         optionC: "this",
         optionD: "const",
-        answer: "optionD"
+        answer: "optionD",
+        isMultChoice: true
     },
     // Question 5
     {
-        prompt: "What of the following is NOT a supported data type in Javascript?",
+        question: "What of the following is NOT a supported data type in Javascript?",
         optionA: "Boolean",
         optionB: "Number",
         optionC: "Algoritm",
         optionD: "Object",
-        answer: "optionC"
+        answer: "optionC",
+        isMultChoice: true
     },
     // Question 6
     {
-        prompt: "Which of the following is used to write a single-line comment in Javascript?",
+        question: "Which of the following is used to write a single-line comment in Javascript?",
         optionA: "$$",
         optionB: "//",
         optionC: "+-+",
         optionD: "<>",
-        answer: "optionB"
+        answer: "optionB",
+        isMultChoice: true
     },
     // Question 7
     {
-        prompt: "Which keyword is used to declare an asynchronous function?",
+        question: "Which keyword is used to declare an asynchronous function?",
         optionA: "async",
         optionB: "null",
         optionC: "await",
         optionD: "asyncFunc",
-        answer: "optionA"
+        answer: "optionA",
+        isMultChoice: true
     },
     // Question 8
     {
-        prompt: "Which function is used to serialize an object into a JSON string in Javascript?",
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
         optionA: "Date()",
         optionB: "toString()",
         optionC: "forEach()",
         optionD: "stringify()",
-        answer: "optionD"
+        answer: "optionD",
+        isMultChoice: true
     },
     // Question 9
     {
-        prompt: "Which of the following is an example of a string in Javascript?",
+        question: "Which of the following is an example of a string in Javascript?",
         optionA: "[maybeString]",
         optionB: "\"anotherPossibility\"",
         optionC: "aString()",
         optionD: "{stringHere}",
-        answer: "optionB"
+        answer: "optionB",
+        isMultChoice: true
     },
     // Question 10
     {
-        prompt: "Which symbol is used to seperate Javascript statements?",
+        question: "Which symbol is used to seperate Javascript statements?",
         optionA: "Colon ( : )",
         optionB: "Period ( . )",
         optionC: "Semicolon ( ; )",
         optionD: "Modulus ( % )",
-        answer: "optionC"
+        answer: "optionC",
+        isMultChoice: true
     },
     
 
